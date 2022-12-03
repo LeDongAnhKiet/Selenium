3f66e69a68ef57fc3f0baac48bcf6aff0655e8eb
unknown <you@example.com>
2022-12-03 15:31:40 +0700

    GgSearch

---
+++ GoogleSearch.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driv = webdriver.Chrome(executable_path='vnev/chromedriver')
+driv.get('https://www.google.com')
+s = input('Nhập: ')
+ctrl = driv.find_element(By.NAME, 'q')
+ctrl.send_keys(s)
+ctrl.submit()
+
+res = driv.find_elements(By.CSS_SELECTOR, 'div.g')
+a = 1
+for r in res:
+    t = r.find_element(By.TAG_NAME, 'a').text
+    l = r.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    a += 1
+    print(t + '\n' + l + '\n\n')
+    if a > 5:
+        break
+
+driv.close()
--- Introduction to Python/Bai04.py
+++ Introduction to Python/Bai04.py
@@ -14,6 +14,13 @@ class Bai4:
     #     with open('data/emp.json', encoding='utf-8') as f:
     #         return json.load(f)
     #
+    n = len(employees)
+
+    def tu_dien(self):
+        for i in range(0, self.n):
+            print('Mã nhân viên: ' + self.employees[i]['ma_nv'].__str__()
+                  + '\nTên nhân viên: ' + self.employees[i]['ten_nv'])
+
     #
     # def save_data(data):
     #     with open('data/emp.json', mode='w', encoding='utf-8') as f:
@@ -28,6 +35,16 @@ class Bai4:
     #             elif k == 'ten_nv':
     #                 print(f'Tên nhân viên: {v}')
     #
+    def search(self, ten):
+        for i in range(0, self.n):
+            if self.employees[i]['ten_nv'].__contains__(ten):
+                print('Mã nhân viên: ' + self.employees[i]['ma_nv'].__str__()
+                      + '\nTên nhân viên: ' + self.employees[i]['ten_nv'])
+                return
+            else:
+                print('Ko có\n')
+                return -1
+
     #
     # def add_emp(data, ma_nv, ten_nv):
     #     e = {
@@ -37,6 +54,18 @@ class Bai4:
     #     data.append(e)
     #     save_data(data)
     #
+    def add(self, ma, ten):
+        for i in range(0, self.n):
+            if self.employees[i]['ten_nv'] == ma:
+                print('Đã tồn tại')
+                return -1
+        emp = {
+            "ma_nv": ma,
+            "ten_nv": ten
+        }
+        self.employees.append(emp)
+        self.n = len(self.employees)
+
     #
     # def delete_emp(data, ma_nv):
     #     for idx, em in enumerate(data):
@@ -47,6 +76,13 @@ class Bai4:
     #     else:
     #         print('Không có nhân viên này.')
     #
+    def delete(self, ten):
+        for i in range(0, self.n):
+            if self.employees[i]['ten_nv'].__contains__(ten):
+                del self.employees[i]
+                self.n = len(self.employees)
+        else:
+            print('Ko có\n')
     #
     # def find_emp(data, kw):
     #     for em in data:
@@ -54,9 +90,18 @@ class Bai4:
     #             print(em['ma_nv'])
     #
     #
-# if __name__ == '__main__':
+
+
+if __name__ == '__main__':
+    b = Bai4()
+    b.tu_dien()
+    b.search('A')
 #     data = load_data()
 #     output(data)
 #     #add_emp(data, 9, 'Trần Thị B')
 #     delete_emp(data, 3)
 #     find_emp(data, 'Văn')
+    ma = int(input('Mã: '))
+    ten = input('Tên: ')
+    b.add(ma, ten)
+    b.delete(ten)
